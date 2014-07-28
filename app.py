2d539d1c04daea3cb0264557325a6d89c2d9fa84
Kevin Moore <kevin@spendship.com>
2014-07-28 17:58:55 -0500

    Retreive file upload as request.files

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
-from image import images
+# from image import images
 
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['DEBUG'] = 1
@@ -12,7 +15,11 @@ db = SQLAlchemy(app)
 # View/handler to drop all the images
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print "request method: %s" % request.method
     if request.method == 'POST':
+        print "data: ", type(request.data)
+        if request.files:
+            print "files: %r" % request.files
         return redirect('/view')
     return render_template('index.html')
 
