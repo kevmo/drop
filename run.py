7f8595d4cdbf8474aeb9600ac4d7e5b14c41841b
Kevin Moore <kevin@spendship.com>
2014-07-28 22:59:05 -0500

    Using Flask-Script manager to run tasks

--- run.py
+++ run.py
@@ -1,29 +1,41 @@
+import os
+
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
 from werkzeug.utils import secure_filename
 
 from core import Service
 
+#
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
+# create and configure app
 app = Flask(__name__)
 app.config['DEBUG'] = 1
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'postgresql+psycopg2://hoth@localhost:5432/drop'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+# instantiate extensions
 db = SQLAlchemy(app)
+manager = Manager(app)
 
+# define model and table
 class Image(db.Model):
     __tablename__ = 'files'
 
     id = db.Column('id', db.Integer, primary_key=True)
-    file = db.Column('file', db.BLOB)
+    file = db.Column('file', db.String(10000))
 
     def __repr__(self):
         return '<file %r>' % self.id
 
 
-class Images_Service(Service):
+# put SQL services on top of model
+class ImagesService(Service):
     __model__ = Image
+
 
 # View/handler to drop all the images
 @app.route('/', methods=['GET', 'POST'])
@@ -34,13 +46,19 @@ def index():
         if request.files:
             #save
             print "multidict: %r" % request.files
-            print "files: %r" % request.files['file1']
+            print "type of files: %r" % type(request.files['file1'])
             try:
                 Images_Service.create(
-
+                    file = request.files['file1']
                 )
+            except:
+                print "Image_Service not working"
         return redirect('/view')
     return render_template('index.html')
+
+@manager.command
+def hello():
+    print "hello"
 
 # Handler to
 @app.route('/view')
@@ -49,4 +67,6 @@ def see_file():
 
 
 if __name__ == '__main__':
-    app.run()
+    db.create_all()
+    manager.run()
+    # app.run()
